113f3a16c66ca4faf1f971e0d3599a9134bf9c10
Moyo Tassimo Stephane <moyo.stephane@gmail.com>
2021-06-20 07:03:09 +0100

    challenge update

---
+++ Data Types.pyy.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+j = 4
+a= 4.0
+b = "HackerRank "
+# Read and save an integer, double, and String to your variables.
+intA=int(input())
+FloatB=float(input())
+stringC=input()
+# Print the sum of both integer variables on a new line.
+print(j+intA)
+# Print the sum of the double variables on a new line.
+print(FloatB + a)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(b + stringC)
