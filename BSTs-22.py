987a9e6d073b0b3188e42a1548fd1feb02d28b7e
Moyo Tassimo Stephane <moyo.stephane@gmail.com>
2021-07-19 20:10:20 +0100

    update

---
+++ BSTs-22.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+    def getHeight(self,root):
+        #Write your code here
+        if root is None:
+            return -1
+        LeftHeight = self.getHeight(root.left)
+        rightHeight = self.getHeight(root.right)
+        
+        if LeftHeight > rightHeight:
+            return 1+LeftHeight
+        else:
+            return 1+rightHeight
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
